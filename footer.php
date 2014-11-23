6f7af6bf2844427688450176efe7d2c4991d6e59
Your Name <diego@brasa.art.br>
2014-11-22 23:05:31 -0200

    ops, agora sim

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-<div id="ftd"><a rel="editora" href="http://www.ftd.com.br"><img alt="FTD Editora" style="border-width:0" src="http://www.atitudeconhecimento.com.br/site/wp-content/themes/atitude-theme/img/logo-ftd.jpg" /></a></div>
+<div id="ftd"><a rel="editora" href="http://www.ftd.com.br"><img alt="FTD Editora" style="border-width:0" src="<?php bloginfo('template_directory'); ?>/img/logo-ftd.jpg" /></a></div>
 <p class="right">
 	<span>Desenvolvido por <a href="http://www.brasa.art.br/" title="Brasa Design e Tecnologia" target="_blank">Brasa</a> com <a href="http://br.wordpress.org" title="WordPress Brasil" target="_blank">WordPress</a></span> 
 </p>
