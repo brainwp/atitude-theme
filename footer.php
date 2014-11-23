4bb0b41e8c51392ec9daf058a222e25319bd6d32
Your Name <diego@brasa.art.br>
2014-11-22 23:02:25 -0200

    colocando a brasa no footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div id="footer">
-<div id="ftd"><a rel="editora" href="http://www.ftd.com.br"><img alt="FTD Editora" style="border-width:0" src="http://www.atitudeconhecimento.com.br/site/wp-content/themes/MistyLook/img/logo-ftd.jpg" /></a></div>
+<div id="ftd"><a rel="editora" href="http://www.ftd.com.br"><img alt="FTD Editora" style="border-width:0" src="http://www.atitudeconhecimento.com.br/site/wp-content/themes/atitude-theme/img/logo-ftd.jpg" /></a></div>
 <p class="right">
-	<span>Desenvolvido por <a href="http://www.etedesign.com.br/" title="Et� Design e Tecnologia" target="_blank">Et&eacute; Design</a> com <a href="http://br.wordpress.org" title="WordPress Brasil" target="_blank">WordPress</a></span> 
+	<span>Desenvolvido por <a href="http://www.brasa.art.br/" title="Brasa Design e Tecnologia" target="_blank">Brasa</a> com <a href="http://br.wordpress.org" title="WordPress Brasil" target="_blank">WordPress</a></span> 
 </p>
 <br class="clear" />
 </div><!-- end id:footer -->
